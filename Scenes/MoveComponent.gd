fb39b40ce9cea19a77793a0624b8cdc73328f5c4
thelazyone <giacomo.pantalone@gmail.com>
2024-05-16 21:45:07 +0200

    more comments

--- Scenes/MoveComponent.gd
+++ Scenes/MoveComponent.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+# The state machine of the movement orders for the actor using this component.
+# This is a work in progress that might change further on.
 enum ActionStates {
 	Idle, 			# Doing Nothing
 	Moving, 		# Reaching point x,y
@@ -8,64 +10,26 @@ enum ActionStates {
 	}
 	
 var state = ActionStates.Idle
+
+# Some states rely on a target, which could be a point on the screen or an object.
+# TODO maybe condense the two in a single enum (or what GDScript allows)?
 var target = Vector2(0, 0)
 var target_obj = null
+
+# Direction and position of the actor on the XZ plane.
 var plane_direction = 0
 var plane_position = Vector2(0, 0)
 
-# Constants. To be updated after this.
+# Parameters visible from outside. To be updated after this.
 @export var SPEED = 200
 @export var ROT_SPEED = 5
 @export var MIN_ANGLE_MOVE = PI / 4
-
-var target_distance_threshold = 0.1
-var interaction_range = 0.5
-
+@export var target_distance_threshold = 0.1
+@export var interaction_range = 0.5
 
 
-# Movement towards target - TODO check for collisions using Godot tools
-func _move_towards(target, delta, threshold):
-	
-	# Resetting velocity to default:
-	get_parent().velocity.x = 0
-	get_parent().velocity.z = 0
-		
-	# If the distance is small enough, skipping the move.
-	if plane_position.distance_to(target) < target_distance_threshold:
-		return false
-	
-	# First rotating if not rotated.
-	var target_direction = plane_position.angle_to_point(target)
-	
-	plane_direction += clamp(Geometry.diff_angles(target_direction, plane_direction), -ROT_SPEED * delta, ROT_SPEED * delta)
-	
-	# If the difference in angle is not excessive
-	if abs(Geometry.diff_angles(target_direction, plane_direction)) < MIN_ANGLE_MOVE:
-		get_parent().velocity.x = SPEED * cos(plane_direction) * delta
-		get_parent().velocity.z = SPEED * sin(plane_direction) * delta
-		
-	# Updating the navigation agent as well.
-	nav_agent.set_velocity(Geometry.plane_to_space(get_parent().velocity))
-	
-	_update_transforms()
-	return true
-
-func _interact_with(target, delta, range):
-	
-	# TODO return false if the interaction is not necessary anymore.
-	
-	return true
-
-func _update_transforms():
-	get_parent().rotation = Vector3(0, -plane_direction, 0)
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	plane_position = Vector2(get_parent().position.x, get_parent().position.z)
-	print("Setting up tank at ", plane_position)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Process function. Called every frame.
+# 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
 	# Updating the plane position and target point:
@@ -99,36 +63,89 @@ func _process(delta):
 		
 	pass
 
+
+# Private functions to handle the movement.
+
+# Movement towards target - TODO check for collisions using Godot tools
+func _move_towards(target, delta, threshold):
+	
+	# Resetting velocity to default:
+	get_parent().velocity.x = 0
+	get_parent().velocity.z = 0
+		
+	# If the distance is small enough, skipping the move.
+	if plane_position.distance_to(target) < target_distance_threshold:
+		return false
+	
+	# First rotating if not rotated.
+	var target_direction = plane_position.angle_to_point(target)
+	
+	plane_direction += clamp(Geometry.diff_angles(target_direction, plane_direction), -ROT_SPEED * delta, ROT_SPEED * delta)
+	
+	# If the difference in angle is not excessive
+	if abs(Geometry.diff_angles(target_direction, plane_direction)) < MIN_ANGLE_MOVE:
+		get_parent().velocity.x = SPEED * cos(plane_direction) * delta
+		get_parent().velocity.z = SPEED * sin(plane_direction) * delta
+		
+	# Updating the navigation agent as well.
+	nav_agent.set_velocity(Geometry.plane_to_space(get_parent().velocity))
+	
+	_update_transforms()
+	return true
+
+
+func _interact_with(target, delta, range):
+	
+	# TODO to implement.
+	
+	var interaction_successful = true
+	# TODO return false if the interaction is not necessary anymore.
+	
+	return interaction_successful
+
+
+# Apply the component's direction to the parent object. Should there be a better way?
+func _update_transforms():
+	get_parent().rotation = Vector3(0, -plane_direction, 0)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	plane_position = Vector2(get_parent().position.x, get_parent().position.z)
+	print("Setting up tank at ", plane_position)
+
+
 # Public Functions Here
+
 func command_move(i_target):
 	state = ActionStates.Moving
 	update_target_location(Geometry.plane_to_space(i_target))
 	print ("Send command Move towards", i_target)
-	
+
+
 func command_interact(i_target_obj):
 	# TODO check if target is a valid target.
 	state = ActionStates.Interacting
 	target_obj = i_target_obj
 	update_target_location(target_obj.position)
 	print ("Send command Interact")
-	
+
+
 func command_stop():
 	state = ActionStates.Idle
 	target = Vector2(0, 0)
 	print ("Send command Stop")
-	
+
+
 func command_follow(i_target_obj):
 	# TODO check if target is a valid target.
 	state = ActionStates.Following
 	target_obj = i_target_obj
 	update_target_location(target_obj.position)
 	print ("Send command Follow")
-	
-	
-	
+
+
 # Pathfinding navigation
 @onready var nav_agent = $NavigationAgent3D
-
 func update_target_location(location):
 	nav_agent.set_target_position(location)
-	
